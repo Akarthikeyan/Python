f510a6278a571670b662182bf6d523a77daefc44
Akarthikeyan <akarthikeyancse@gmail.com>
2014-01-20 19:16:50 +0530

    Create operatoroverload.py
    to overload the operator(+)

---
+++ operatoroverload.py
@@ -0,0 +1,8 @@
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=[[2,4,6],[3,6,9],[2,4,8]]
+for i in range(3):
+  for j in range(3):
+    for k in range(3):
+      c+=a[i][j]*b[i][j]
+    print(" ",c,end='')
+  print()
